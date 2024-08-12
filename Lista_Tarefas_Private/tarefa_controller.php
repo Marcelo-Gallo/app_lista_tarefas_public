e47a902b19ff40c93fd4329b52fa57a3344323f9
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-08-12 20:09:34 -0300

    PHP - app lista de tarefas - atualização tarefa_controller.php disponibilizada no repositório

--- Lista_Tarefas_Private/tarefa_controller.php
+++ Lista_Tarefas_Private/tarefa_controller.php
@@ -5,17 +5,24 @@
     require "../../app_lista_tarefas/tarefa.service.php";
     require "../../app_lista_tarefas/conexao.php";
 
-    //nova instancia do objeto tarefa
-    $tarefa = new Tarefa();
-
-    //set mágico (tarefa.model.php)
-    $tarefa->__set('tarefa', $_POST['tarefa']);
-
-    $conexao = new Conexao();
-
-    $tarefaService = new TarefaService($conexao, $tarefa);
-    $tarefaService->inserir();
+    //se houver um indice acao setado na superglobal get, ele é atribuido à variavel $acao, caso contrario a aplicação vai esperar uma variavel chamada $acao
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
     
-    //está no contexto publico, então não há necessidade de formatar o caminho
-    header('Location: nova_tarefa.php?inclusao=1');
+    if( $acao == 'inserir') {
+        //nova instancia do objeto tarefa
+        $tarefa = new Tarefa();
+
+        //set mágico (tarefa.model.php)
+        $tarefa->__set('tarefa', $_POST['tarefa']);
+
+        $conexao = new Conexao();
+
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
+        
+        //está no contexto publico, então não há necessidade de formatar o caminho
+        header('Location: nova_tarefa.php?inclusao=1');
+    } else if($acao == 'recuperar'){
+        echo 'Chegamos até aqui!';
+    }
 ?>
